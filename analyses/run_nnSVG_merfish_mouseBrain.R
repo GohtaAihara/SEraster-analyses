19f535396db5845232ea9e2721b442213c6800d3
Gohta Aihara <gohta.aihara@gmail.com>
2023-09-19 17:59:27 -0400

    updated merfish mouse brain analysis

--- analyses/run_nnSVG_merfish_mouseBrain.R
+++ analyses/run_nnSVG_merfish_mouseBrain.R
@@ -17,8 +17,59 @@ library(here)
 
 par(mfrow=c(1,1))
 
+dataset_name <- "merfish_mouseBrain"
+
 # Load dataset ------------------------------------------------------------
 
 spe <- readRDS(file = "outputs/merfish_mouseBrain_preprocessed.RDS")
 
 plot(spatialCoords(spe), pch=".")
+
+
+# Run method --------------------------------------------------------------
+
+
+# Plot --------------------------------------------------------------------
+
+res <- list("singlecell", 50, 100, 250, 500)
+
+for (i in res) {
+  if (i == "singlecell") {
+    ## plot
+    df <- data.frame(x = spatialCoords(spe)[,1], y = spatialCoords(spe)[,2], transcripts = colSums(assay(spe, "counts")))
+    plt <- ggplot(df, aes(x = x, y = y, col = transcripts)) +
+      coord_fixed() +
+      geom_point(size = 0.1) +
+      scale_color_viridis_c() +
+      labs(title = "Single cell") +
+      theme_bw() +
+      theme(
+        legend.position="none",
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+      )
+  } else {
+    ## rasterize
+    spe_rast <- SEraster::rasterizeGeneExpression(spe, assay_name = "counts", resolution = i, fun = "sum", BPPARAM = BiocParallel::MulticoreParam())
+    
+    ## plot
+    df <- data.frame(x = spatialCoords(spe_rast)[,1], y = spatialCoords(spe_rast)[,2], transcripts = colSums(assay(spe_rast)))
+    plt <- ggplot(df, aes(x = x, y = y, fill = transcripts)) +
+      coord_fixed() +
+      geom_tile() +
+      scale_fill_viridis_c() +
+      labs(title = paste0("Resolution = ", i, " um")) +
+      theme_bw() +
+      theme(
+        legend.position="none",
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+      )
+  }
+  ## save plot
+  ggsave(plot = plt, filename = here("plots", paste0(dataset_name, "_tot_counts_", i, ".pdf")))
+}
