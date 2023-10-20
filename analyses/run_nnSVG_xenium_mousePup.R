ca14a7c068ab28e871df46109b4bd8457e2038a9
Gohta Aihara <gohta.aihara@gmail.com>
2023-10-20 12:41:46 -0400

    evaluate ct specific SVGs for xenium mouse pup

--- analyses/run_nnSVG_xenium_mousePup.R
+++ analyses/run_nnSVG_xenium_mousePup.R
@@ -153,6 +153,72 @@ ggsave(filename = here("plots", dataset_name, paste0(dataset_name, "_nnsvg_globa
 ## load DEG analysis
 df_deg <- read.csv("~/Library/CloudStorage/OneDrive-JohnsHopkins/JEFworks Gohta Aihara/Data/Xenium_mousePup/analysis/diffexp/gene_expression_graphclust/differential_expression.csv")
 
+## Figure (number of s.s. SVG)
+df <- readRDS(file = here("outputs", paste0(dataset_name, "_nnsvg_ct_specific_v1.RDS")))
+alpha <- 0.05
+df <- df %>%
+  mutate(
+    resolution = factor(resolution, levels = c(50, 100, 200, 400)),
+    cluster = factor(cluster, levels = levels(ct_labels))
+  )
+df$svg_boolean <- df$padj <= alpha
+
+# check which clusters were analyzed in each resolution
+for (res in unique(df$resolution)) {
+  ## print out analyzed clusters
+  print(paste0("Resolution: ", res))
+  print(paste0(unique(df[df$resolution == res,]$cluster)))
+}
+
+## number of s.s. SVGs identified in each cluster for each resolution
+df_num_svg <- df %>%
+  group_by(resolution, cluster) %>%
+  summarise(num_svg = sum(svg_boolean), num_pixels = mean(num_pixels))
+ggplot(df_num_svg, aes(x = cluster, y = resolution, size = num_svg, col = log10(num_pixels))) +
+  geom_point() +
+  scale_color_viridis_c() +
+  labs(title = "Number of SVGs",
+       x = "Cluster",
+       y = "Resolution",
+       size = "Number of SVGs",
+       col = "log10(Number of pixels)") +
+  theme_bw()
+## save plot
+ggsave(filename = here("plots", dataset_name, paste0(dataset_name, "_num_svg.pdf")), width = 10, height = 4, dpi = 300)
+
+## Figure (visual inspection of top 25 s.s. SVGs for each cluster)
+## load relevant analysis results
+df <- readRDS(file = here("outputs", paste0(dataset_name, "_nnsvg_ct_specific_v1.RDS")))
+alpha <- 0.05
+df <- df %>%
+  mutate(
+    resolution = factor(resolution, levels = c(50, 100, 200, 400)),
+    cluster = factor(cluster, levels = levels(ct_labels))
+  )
+df$svg_boolean <- df$padj <= alpha
+
+res <- 200
+df_sub <- df[df$resolution == res,]
+
+## load relevant rasterized data
+
+for (ct_label in unique(df_sub$cluster)) {
+  temp <- df_sub[df_sub$cluster == ct_label,]
+  
+  num_rank <- 25
+  df_ranked <- temp[order(temp$rank, decreasing = FALSE),]
+  top_svgs <- df_ranked[1:num_rank,]$gene
+  df_plt <- data.frame(x = spatialCoords(spe_rast)[,1], y = spatialCoords(spe_rast)[,2], as.matrix(t(assay(spe_rast)[top_svgs,]))) %>%
+    pivot_longer(!c(x, y), names_to = "gene", values_to = "exp")
+  ggplot(df_plt, aes(x = x, y = y, fill = exp)) +
+    facet_wrap(~ gene) +
+    geom_tile() +
+    scale_fill_viridis_c(name = "gene expression") +
+    labs(title = paste0("Top ", num_rank, " SVGs based on nnSVG rank")) +
+    theme_bw()
+  ggsave(filename = here("plots", dataset_name, paste0(dataset_name, "_nnsvg_global_top_", num_rank, "_svgs.pdf")), width = 8, heigh = 8, dpi = 300)
+  
+}
 
 # Further exploration -----------------------------------------------------
 
