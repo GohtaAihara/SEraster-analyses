836adeb54adc0200080057ec1791659e03d1f0b6
Gohta Aihara <gohta.aihara@gmail.com>
2023-12-13 17:12:45 -0500

    test plotRaster function

--- analyses/run_CooccurrenceAffinity_codex_humanSpleen.R
+++ analyses/run_CooccurrenceAffinity_codex_humanSpleen.R
@@ -579,3 +579,14 @@ for (i in seq_along(niches)) {
 }
 
 ## runtime
+
+
+# Test -------------------------------------------------------------------
+
+res <- 100
+spe_rast <- readRDS(file = here("outputs", paste0(dataset_name, "_rasterized_resolution_", res, "_binarized.RDS")))
+ct_label <- "Fol B cells"
+
+plotRaster(spe_rast, assay_name = "pixelval", feature_name = "Fol B cells", name = "cell-type counts", option = "inferno")
+plotRaster(spe_rast, assay_name = "re", feature_name = "Fol B cells", name = "RE", option = "inferno")
+plotRaster(spe_rast, assay_name = "bin", feature_name = "Fol B cells", factor_levels = c(0,1), name = "binarized", option = "inferno")
